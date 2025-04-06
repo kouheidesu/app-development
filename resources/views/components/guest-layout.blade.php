c766c82eed74222280e23539669e39f9d2a89388
gyarmex <gyarmex@gmail.com>
2025-04-06 15:23:17 +0900

    Fix guest-layout component error

--- resources/views/components/guest-layout.blade.php
+++ resources/views/components/guest-layout.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.app')
+<!-- resources/views/components/guest-layout.blade.php -->
 
-@section('content')
-<p>Hello, World!</p>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Guest Layout</title>
+</head>
+
+<body>
+    <!-- ここにslotが展開される -->
+    {{ $slot }}
+</body>
+
+</html>
